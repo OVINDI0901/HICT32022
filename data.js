3294e85fe3b1894c86bef88e55a20f8fca0ce45d
thilini <thilinisandeepani514@gmail.com>
2026-07-03 14:17:55 +0530

    feat: setup basic node server and data route

---
+++ data.js
@@ -0,0 +1,6 @@
+const users = [
+{ id: 1, name: "Thilini Sandeepani", role: "Admin" },
+{ id: 2, name: "Student", role: "User" }
+];
+// This line allows other files to use this data (Code Reuse!)
+module.exports = users;
