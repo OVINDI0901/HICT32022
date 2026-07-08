a5f1fadc659228bb898950fd35dcd77b339b7d4a
Ovindi Ishcharya <ovindiishcharya63@gmail.com>
2026-07-08 12:52:51 +0530

    style: update content in data.js

--- data.js
+++ data.js
@@ -1,7 +1,7 @@
 const users = [
-    { id: 1, name: "Thilina Sandeepani", email: "thilina@example.com" },
+    { id: 1, name: "Ovindi ", email: "ovindi@example.com" },
     { id: 2, name: "John Doe", email: "john@example.com" },
-    { id: 3, name: "Ovindi (Team Member)", email: "ovindi@example.com" } // <-- Menna me aluth line eka api ekathu kala!
+
 ];
 
 module.exports = users; 
